291b9b88138a16cfb60ed73b5d7accc8eaf90bdf
Anu Anand <aanand.monsanto.com>
2015-11-20 12:40:51 +0530

    My File

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,51 @@
-## Put comments here that give an overall description of what your
-## functions do
+##This function will take the inverse of a matrix and then cache it as the computation will take long time to calculate.
 
-## Write a short comment describing this function
+##In this function we have used <<- operator which is used to assign the matrix value to an object into a object 
+##that is different from current environment.
+##Below function will create a matrix "MakeCacheMatrix" to set the value of the metrix
+##Set Inverse of the martix and GET Inverse of the matrix
 
-makeCacheMatrix <- function(x = matrix()) {
 
+makeCacheMatrix <-function(x = matrix()) {
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setsolve <- function(solve) m <<- solve
+        getsolve <- function() m
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the MakeCacheMatrix
+## If the inverse has already been calculated and the matrix is already created the it will return to NULL by skipping the computation otherwise
+## it will inverse the Matrix and stores the value in the cache via the Setsolve function.
+
+
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ## Rget inverse of a metrix
+        m <- x$getsolve()
+        # Returns the inverse if the inverse is already calculated
+        if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+        }
+        #computes inverses of a matrix if it is a new matrix
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setsolve(m)
+        m
 }
+
+y = matrix(1:4,2,2)
+y
+
+a<-makeCacheMatrix(y)
+a
+
+cacheSolve(a)
